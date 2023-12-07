563c17db09b1997577194a8a9778a7b781ce43ae
masterblaster42069 <114529314+masterblaster42069@users.noreply.github.com>
2023-12-07 15:54:47 +0100

    Add project files.

---
+++ BossFight/GameCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BossFight;
+
+namespace BossFight
+{
+    internal class GameCharacter
+    {
+        public int Health;
+        public int Strenght;
+        public int Stamina;
+        private int maxStam;
+
+        public GameCharacter(int health, int strenght, int stamina)
+        {
+            Health = health;
+            Strenght = strenght;
+            Stamina = stamina;
+            maxStam = stamina;
+        }
+
+        public void Fight()
+        {
+            Stamina -= 10;
+        }
+
+        public void Recharge()
+        {
+            Stamina = maxStam;
+        }
+    }
+}
---
+++ BossFight/Program.cs
@@ -0,0 +1,25 @@
+﻿using BossFight;
+namespace BossFight
+{
+    public class Game
+    { 
+        public void Start()
+        {
+            GameCharacter Hero = new GameCharacter(100, 20, 40);
+            GameCharacter Boss = new GameCharacter(400, 30, 10);
+
+            Console.WriteLine("1. Attack");
+            Console.WriteLine("2. Recharge");
+            var input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                Hero.Fight();
+                Boss.Fight();
+            }
+        }    
+    } 
+} 
+
+
+
