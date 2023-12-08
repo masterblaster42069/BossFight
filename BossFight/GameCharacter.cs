c68591dc060839a068c3be8f217b4270c2a5e810
masterblaster42069 <114529314+masterblaster42069@users.noreply.github.com>
2023-12-08 13:27:30 +0100

    FINALLY :))))

--- BossFight/GameCharacter.cs
+++ BossFight/GameCharacter.cs
@@ -4,32 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BossFight;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BossFight
 {
     internal class GameCharacter
     {
+
+
+        public string Name;
         public int Health;
-        public int Strenght;
+        public int Strength;
         public int Stamina;
         private int maxStam;
+       
 
-        public GameCharacter(int health, int strenght, int stamina)
-        {
+        public GameCharacter(string name, int health, int strength, int stamina)
+        { 
+            Name = name;
             Health = health;
-            Strenght = strenght;
+            Strength = strength;
             Stamina = stamina;
             maxStam = stamina;
+ 
         }
 
-        public void Fight()
+        public void Fight(GameCharacter opponent)
         {
-            Stamina -= 10;
+            Random random = new Random();
+            int bossDamage = random.Next(31);
+
+            if (Stamina == 0)
+            {
+                Console.WriteLine("Recharge first");
+            }
+            else
+            {
+                int damage = Math.Min(Strength, opponent.Health);
+                opponent.Health -= damage;
+
+                Console.WriteLine($"{Name} hit {opponent.Name} with {damage} damage, " +
+                                  $"{opponent.Name} now has {opponent.Health} health left.");
+
+                Stamina -= 10;
+
+               
+            }
         }
 
         public void Recharge()
         {
             Stamina = maxStam;
+            Console.WriteLine($"{Name} is Recharging, they can still be attacked");
         }
     }
 }
--- BossFight/Program.cs
+++ BossFight/Program.cs
@@ -3,21 +3,67 @@ namespace BossFight
 {
     public class Game
     { 
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
         public void Start()
         {
-            GameCharacter Hero = new GameCharacter(100, 20, 40);
-            GameCharacter Boss = new GameCharacter(400, 30, 10);
+            Random random = new Random();
+            
+            GameCharacter Hero = new GameCharacter("Hero",100, 20, 40);
+            GameCharacter Boss = new GameCharacter("Boss",400, 0, 10);
 
-            Console.WriteLine("1. Attack");
-            Console.WriteLine("2. Recharge");
-            var input = Console.ReadLine();
-
-            if (input == "1")
+            while (Hero.Health > 0 && Boss.Health > 0)
             {
-                Hero.Fight();
-                Boss.Fight();
+                Boss.Strength = random.Next(31);
+                Console.WriteLine("1. Attack");
+                Console.WriteLine("2. Recharge");
+                var input = Console.ReadLine();
+
+                if (Boss.Stamina == 0)
+                {
+                    Boss.Recharge();
+                }
+                else
+                {
+                    Boss.Fight(Hero);
+                }
+
+                if (input == "1")
+                {
+                    Hero.Fight(Boss);
+
+                    if (Boss.Health < 0) 
+                    {
+                        Boss.Fight(Hero);
+                    }
+                }
+                else if (input == "2")
+                {
+                    if (Boss.Stamina == 0)
+                    {
+                        Boss.Recharge();
+                        Console.WriteLine($"{Boss.Name} is recharging stamina.");
+                    }
+                    else
+                    {
+                        Boss.Fight(Hero);
+                    }
+
+                    Hero.Recharge();
+                }
             }
-        }    
+            if (Hero.Health < 0)
+                {
+                Console.WriteLine("Hero is the Winner!");
+                }
+            else
+                {
+                Console.WriteLine("Boss Won");
+                }
+        }
     } 
 } 
 
